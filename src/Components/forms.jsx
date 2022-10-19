900c332c8888cc07a328e718aa4f39869d4e6016
Blen Hailu <belaki2@gmail.com>
2022-10-20 01:52:20 +0300

    1. add on submit button 2. add css to the form 3. delete uncecessary files

---
+++ src/Components/forms.jsx
@@ -0,0 +1,84 @@
+import {useState} from 'react';
+//import useLocalStorageState from 'use-local-storage-state';
+
+function BasicForm(){
+
+  const [fullName, setFullName] =useState('')
+
+  /* const [fullName, setFullName] =useLocalStorageState(
+    'text',
+    {defaultValue: ""}
+    );
+ */
+  const handleFullNameChange = event => {
+    setFullName(event.target.value)
+  };
+
+  const [email, setEmail] = useState('')
+  
+  const handleEmailChange = event => {
+    setEmail(event.target.value)
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('Thank you!');
+
+  };
+
+  
+
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Enter Full Name"
+              onChange={handleFullNameChange}
+              value={fullName}
+            />
+            <label>Email address</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={handleEmailChange}
+              value={email}
+            />
+          </div>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+    </div>
+  )
+}
+export default BasicForm;
+
+
+
+
+/* function Forms (){
+
+const { register, handleSubmit, errors} = useForm()
+
+const onSubmit = (data) => {
+  console.log(data)
+}
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <input type = "text" placeholder = "Name" name= "name" ref={register}/>
+    <input type = "text" placeholder = "Email" name= "email" ref={register({required: true})}/>
+    {errors.email && <p> Email required</p>}
+    <input type = "submit"/>
+
+  </form>
+)
+} 
+export default Forms; */
+
+
