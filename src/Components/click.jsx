556bda166cdde6aa9e5055a539d50c5ca4c4b79c
Blen Hailu <belaki2@gmail.com>
2022-11-02 14:54:43 +0200

    Add component Footer

---
+++ src/Components/click.jsx
@@ -0,0 +1,49 @@
+import {useState} from "react";
+
+export default function MailingForm() {
+  const [fullName, setFullName] =useState('')
+
+  const handleFullNameChange = event => {
+    setFullName(event.target.value)
+  };
+
+  const [email, setEmail] = useState('')
+  
+  const handleEmailChange = event => {
+    setEmail(event.target.value)
+  };
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('Thank you for subscrbing to our newsletter!');
+
+  };
+
+return (
+    <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Enter Full Name"
+              onChange={handleFullNameChange}
+              value={fullName}
+            />
+            <label>Email address</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={handleEmailChange}
+              value={email}
+            />
+            </div>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+    </div>
+  )
+}
