b1e0b9d33cfa6bbb91db5497cc2a432df10978ef
Blen Hailu <belaki2@gmail.com>
2022-11-02 14:34:31 +0200

    add Footer component

---
+++ src/Components/footer.jsx
@@ -0,0 +1,22 @@
+export default function Footer (){
+  return (
+    <nav className ="footer">
+      <ul>
+        <li>
+          <a href="/About Us">About Us</a>
+         </li>
+        <li>
+          <a href="/Help Center">Services</a>
+        </li>
+        <li>
+          <a href="/Terms and Conditions">Terms and Conditions</a>
+        </li>
+        <li>
+          <a href= "/Privacy Policy">Privacy Policy</a>
+        </li>
+      </ul>
+
+    </nav>
+    
+  );
+}
