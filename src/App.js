c633bb258a6572481ff7f048bbbd0e4ccc415bfa
Blen Hailu <belaki2@gmail.com>
2022-11-02 14:22:37 +0200

    Add footer component fix divs

--- src/App.js
+++ src/App.js
@@ -4,62 +4,52 @@ import './App.css';
 import Menubar from './Components/menubar';
 import Subscribe from './Components/subscribe';
 import BasicForm from './Components/forms';
+import Footer from './Components/footer';
 import './App.css';
 
 function App() {
-  return (
+return (
    <>
 
+        <div>
+            <Menubar/>
+        </div>
 
-      <div>
-          <Menubar/>
-      </div>
-
-      < div className="Pagelayout">
-
+        < div className="Pagelayout">
 
             <div className ="Geninfo">
-              <p>
-                  Welcome to KOTIBET! <br></br> 
-                  Kotibet is a platform where you can find all home care services in one place to make your home living experience easy, enjoyable and stress free by providing services ranging from customized shopping, food prepping, cooking, appliance fixing, electricity works, plumbing, landscaping, home cleaning etc.<br></br>
-                  KOTIBET has the ambitious goal of launching the MVP/MSP by the end of 2022 and acquisition of registered users and professional freelancers on the platform.<br></br>
-                  A trusted Partner For Your Ideal Home Servces.<br></br>
+                <p>
+                Welcome to KOTIBET! <br></br> 
+                Kotibet is a platform where you can find all home care services in one place to make your home living experience easy, enjoyable and stress free by providing services ranging from customized shopping, food prepping, cooking, appliance fixing, electricity works, plumbing, landscaping, home cleaning etc.<br></br>
+                KOTIBET has the ambitious goal of launching the MVP/MSP by the end of 2022 and acquisition of registered users and professional freelancers on the platform.<br></br>
+                A trusted Partner For Your Ideal Home Servces.<br></br>
                 </p>
             
-            </div>
-
-            <div>
                 <Subscribe/> 
             </div>
 
             <div className = "Halfpage">
 
-                        <div className ="tocontact">
-                            <p> Contact us to get the best offers! </p>
-                        </div>
+                <div className ="tocontact">
+                    <p> Contact us to get the best offers! </p>
+                </div>
 
-                        <div className='Form'>
-                                <BasicForm/>
-                        </div>
+                <div className='Form'>
+                <BasicForm/>
+                </div>
             </div>
-
-                    
-
-
-        </div>    
-
-
-      
+        </div>
         
+        <div>
+        <Footer/>
+        </div>
+
         
-        
-     </>
+    </>
+)
 
 
-      )
-
-  
-};    
+}
 
 
 export default App;
---
+++ src/Components/footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer (){
+  return (
+    <nav className ="footer">
+      <ul>
+        <li>
+        <a href="/Terms and Conditions">Terms and Conditions</a>
+         </li>
+        <li>
+          <a href="/Privacy Policy">Services</a>
+        </li>
+      </ul>
+
+    </nav>
+    
+  );
+};
