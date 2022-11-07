14266963e3531221710396d97d05b28158853743
Blen Hailu <belaki2@gmail.com>
2022-11-07 07:57:52 +0200

    edited test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to Kotibet/i);
+  const linkElement = screen.getByText(/Contact us to get the best offers!/i);
   expect(linkElement).toBeInTheDocument();
 });
