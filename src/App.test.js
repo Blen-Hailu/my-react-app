e077a374ac5480b927cdeb111790b0bb02e005b7
Blen Hailu <belaki2@gmail.com>
2022-11-08 10:46:30 +0200

    fix error

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import React from 'react';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Contact us to get the best offers!/i);
